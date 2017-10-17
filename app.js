312a8c0dfaadc9b5f6ec3cf2ae3f7dc64b13220b
Ruber <ruber19@gmail.com>
2017-10-16 21:17:25 -0500

    v36

--- app.js
+++ app.js
@@ -11,21 +11,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
-//app.set('appPath', path.join(path.normalize(`${__dirname}/../../..`), 'client/dist'));
-app.set('appPath', path.join(path.normalize(`${__dirname}/../../../../..`), '/client/dist'));
+app.set('appPath', path.join(path.normalize(`${__dirname}`), '/client/dist'));
 
 app.use(express.static(app.get('appPath')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(`${app.get('appPath')}/index.html`));
 });
-
-/*
-app.get('/', function(req, res) {
-   const index = path.join(__dirname, 'client/dist', 'index.html');
-   res.sendFile(index);
-});
-*/
 
 app.use('/api', api)
 
