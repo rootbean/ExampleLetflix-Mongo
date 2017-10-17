7d84ce95b979fb63e89779486322ccd22296db04
Ruber <ruber19@gmail.com>
2017-10-16 20:58:17 -0500

    v32

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.set('appPath', path.join(path.normalize(`${__dirname}/../../..`), 'client/dist'));
+//app.set('appPath', path.join(path.normalize(`${__dirname}/../../..`), 'client/dist'));
+app.set('appPath', path.join(path.normalize(`${__dirname}/../..`), 'client/dist'));
+
 app.use(express.static(app.get('appPath')));
 
 app.get('/', (req, res) => {
